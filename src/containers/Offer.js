370ecef2db2ca6f781d4cf5aaf93db73b4ff0bdd
Laura <laura.ricchi@users.noreply.github.com>
2020-03-29 18:51:56 +0200

    css containers edited

--- src/containers/Offer.js
+++ src/containers/Offer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { Helmet } from "react-helmet";
+import "../assets/css/Offer.css";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
@@ -30,28 +31,36 @@ const Offer = () => {
 
   return (
     <>
+      <Helmet>
+        <title>{data.title}</title>
+      </Helmet>
       {isLoading === true ? (
         <p>En cours de chargement ...</p>
       ) : (
-        <>
-          <div className="offer">
-            <img alt="pictures" src={data.pictures[0]} />
-            <div className="offer-title">{data.title}</div>
-            <div className="offer-price">{data.price}€</div>
-            <div className="offer-date">
-              <Moment format="DD/MM/YYYY à HH:mm">{data.created}</Moment>
+        <div className="container-offer">
+          <div className="container-picture-price">
+            <img className="picture-offer" alt="offer" src={data.pictures[0]} />
+
+            <div className="container-info-offer">
+              <div className="offer-title">{data.title}</div>
+              <div className="offer-price">{data.price}€</div>
+              <div className="offer-date">
+                <Moment format="DD/MM/YYYY à HH:mm">{data.created}</Moment>
+              </div>
             </div>
           </div>
-          <div className="offer-description">
-            <p>Description</p>
+          <div className="container-offer-description">
+            <p className="descr">Description</p>
             <div className="offer-description">{data.description}</div>
           </div>
 
           <div className="offer-creator">
-            <div className="offer-username">
-              {data.creator.account.username}
+            <div className="infos-user">
+              <div className="offer-username">
+                {data.creator.account.username}
+              </div>
             </div>
-            )}
+
             {/* <button onClick={() => {
               if (user === null) {
                 <Redirect to="/log_in"/>
@@ -60,15 +69,15 @@ const Offer = () => {
                 history.push("/Payment", 
                 {title: data.title},
                 {price : data.price}) */}
-            <button onClick={() => {}}>
-              <div className="icon-offer">
-                <FontAwesomeIcon icon={faCartPlus} />
 
+            <div className="buy-offer">
+              <button className="button-buy-offer" onClick={() => {}}>
+                <FontAwesomeIcon icon={faCartPlus} />
                 <span>Acheter</span>
-              </div>
-            </button>
+              </button>
+            </div>
           </div>
-        </>
+        </div>
       )}
     </>
   );
