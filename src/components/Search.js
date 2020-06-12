57ca840948c47618d69f1fd15d1ad67ddb22e840
Laura <laura.ricchi@users.noreply.github.com>
2020-06-12 23:38:34 +0200

    search media queries edited

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,8 +13,8 @@ const Search = ({ setData }) => {
   };
 
   return (
-    <div className="container-ellipsis-">
-      <div className="ellipsis">
+    <div className="container">
+      <div className="container-ellipsis">
         <div className="container-form">
           <div className="search-form">
             <form onSubmit={handleSubmit}>
